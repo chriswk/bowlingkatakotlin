ca4e7b5a4cc2df4f90c3d6e5d44b208a5574cee3
Christopher Kolstad <git@chriswk.no>
2020-09-19 18:45:56 +0200

    Handles scoring

--- src/main/kotlin/com/chriswk/bowling/BowlingGame.kt
+++ src/main/kotlin/com/chriswk/bowling/BowlingGame.kt
@@ -1,5 +1,29 @@
 package com.chriswk.bowling
 
-class BowlingGame(scores : String) {
-    val score = 0
+class BowlingGame(val scores : String) {
+    private val rolls: IntArray = parseRolls().toIntArray()
+    val score = scoreGame()
+
+    private fun scoreGame(): Int {
+        return (1..10).fold((0 to 0)) { (sum, rollIdx), frame ->
+            if(rolls[rollIdx] == 10) {
+                (sum + (strike(rollIdx)) to rollIdx + 1)
+            } else {
+                val frameScore = rolls[rollIdx] + rolls[rollIdx+1]
+                if (frameScore == 10) {
+                    ((sum + spare(frameScore, rollIdx+2)) to rollIdx + 2)
+                } else {
+                    (sum + frameScore) to rollIdx + 2
+                }
+            }
+        }.first
+    }
+
+    private fun strike(rollIdx: Int): Int {
+        return rolls[rollIdx] + (rolls.getOrNull(rollIdx+1) ?: 0) + (rolls.getOrNull(rollIdx+2) ?: 0)
+    }
+    private fun spare(score: Int, rollIdx : Int): Int {
+        return score + (rolls.getOrNull(rollIdx) ?: 0)
+    }
+    private fun parseRolls(): List<Int> = scores.split(",").map { it.toInt(10) }
 }
--- src/test/kotlin/com/chriswk/bowling/GameTest.kt
+++ src/test/kotlin/com/chriswk/bowling/GameTest.kt
@@ -7,8 +7,53 @@ class GameTest {
 
     @Test
     fun `all zeros score 0`() {
-        val allZeros = (1..10).map { "0,0" }.joinToString(separator = ",")
+        val allZeros = (1..20).map { "0" }.joinToString(separator = ",")
         val game = BowlingGame(allZeros)
         assertThat(game.score).isEqualTo(0)
     }
+
+    @Test
+    fun `all threes score 60`() {
+        val allThrees = (1..20).map { "3" }.joinToString(separator = ",")
+        val game = BowlingGame(allThrees)
+        assertThat(game.score).isEqualTo(60)
+    }
+
+    @Test
+    fun `frames of nine score 90`() {
+        val framesOfNine = (1..10).map { "4,5" }.joinToString(separator = ",")
+        val game = BowlingGame(framesOfNine)
+        assertThat(game.score).isEqualTo(90)
+    }
+
+    @Test
+    fun `all spares and nines score 199`() {
+        val sparesAndNine = (1..10).map { "9,1" }.joinToString(separator = ",") + ",9"
+        val game = BowlingGame(sparesAndNine)
+        assertThat(game.score).isEqualTo(190)
+    }
+
+    @Test
+    fun `alternating spares and strikes score 200`() {
+        val game = BowlingGame("5,5,10,5,5,10,5,5,10,5,5,10,5,5,10,5,5")
+        assertThat(game.score).isEqualTo(200)
+    }
+
+    @Test
+    fun `A game of all strikes scores 300`() {
+        val allStrikes = (1..12).map { "10" }.joinToString(separator = ",")
+        assertThat(BowlingGame(allStrikes).score).isEqualTo(300)
+    }
+    @Test
+    fun `A game with alternating strikes and gutters scores 50`() {
+        val strikeAndGutter = "10,0,0,10,0,0,10,0,0,10,0,0,10,0,0"
+        assertThat(BowlingGame(strikeAndGutter).score).isEqualTo(50)
+    }
+
+    @Test
+    fun `A game with a strike in the ninth and two strikes and nine in the tenth scores 59`() {
+        val oddBall = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,10,10,9"
+        assertThat(BowlingGame(oddBall).score).isEqualTo(59)
+    }
+
 }
